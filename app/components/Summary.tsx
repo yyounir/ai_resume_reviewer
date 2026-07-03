a7aed0478208f213a9241d34a6fc995379973aba
yyounir <yyproton168@icloud.com>
2026-07-03 00:40:36 -0400

    0.6 - Added detailed feedback to the resume feedback in the Resume page - The resume image has been adjusted to fit the page

--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,7 +1,40 @@
-const Summary = () => {
+import ScoreBadge from "./ScoreBadge";
+import ScoreGauge from "./ScoreGauge";
+
+const Category = ({title, score} : {title: string, score: number}) => {
+    const textColor = score > 70 ? 'text-green-600' : score > 49 ? 'text-yellow-600' : 'text-red-600';
+
     return (
-        <div>
-            Summary
+        <div className="resume-summary">
+            <div className="category">
+                <div className="flex flex-row gap-2 items-center justify-center">
+                    <p className="text-2xl">{title}</p>
+                    <ScoreBadge score={score} />
+                </div>
+                <p className="text-2xl">
+                    <span className={`${textColor} font-bold`}>{score}</span>/100
+                </p>
+            </div>
+        </div>
+    )
+}
+
+const Summary = ({feedback} : {feedback: Feedback}) => {
+    return (
+        <div className="bg-white rounded-2xl shadow-md w-full">
+            <div className="flex flex-row items-center p-4 gap-8">
+                <ScoreGauge score={feedback.overallScore} />
+
+                <div>
+                    <h2 className="text-2xl font-bold">Your Resume Score</h2>
+                    <p className="text-sm text-gray-500">This score is calculated based on the categories listed below.</p>
+                </div>
+            </div>
+
+            <Category title="Tone & Style" score={feedback.toneAndStyle.score} />
+            <Category title="Content" score={feedback.content.score} />
+            <Category title="Structure" score={feedback.structure.score} />
+            <Category title="Skills" score={feedback.skills.score} />
         </div>
     )
 }
--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -65,16 +65,15 @@ const Resume = () => {
                 </Link>
             </nav>
             <div className="flex flex-row w-full max-lg:flex-col-reverse">
-                <section className="feedback-section bg-[url('/images/bg-small.svg')] bg-cover h-[100vh] sticky top-0 items-center justify-center">
+                <section className="feedback-section bg-[url('/images/bg-small.svg') bg-cover h-[100vh] sticky top-0 items-center justify-center">
                     {imageUrl && resumeUrl && (
-                        <div className=" animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl: h-fit w-fit">
+                        <div className="animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit">
                             <a href={resumeUrl} target="_blank" rel="noopener noreferrer">
-                                <img 
-                                    src = {imageUrl}
-                                    className = "w-full h-full object-contain rounded-2xl"
+                                <img
+                                    src={imageUrl}
+                                    className="w-full h-full object-contain rounded-2xl"
                                     title="resume"
                                 />
-                                
                             </a>
                         </div>
                     )}
